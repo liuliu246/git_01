386362550b26e33c64185f4c4f34d95609e75243
liuhong <972131119@qq.com>
2021-06-01 20:35:32 +0800

    second commit

--- src/main/java/cn/list_01/gitTest.java
+++ src/main/java/cn/list_01/gitTest.java
@@ -3,5 +3,6 @@ package cn.list_01;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git1");
     }
 }
