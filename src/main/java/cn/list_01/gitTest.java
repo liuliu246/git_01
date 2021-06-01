4d8e52f0f20171a298eca819d5857e48e712eae8
liuliu246 <58115429+liuliu246@users.noreply.github.com>
2021-06-01 21:06:37 +0800

    pull1

--- src/main/java/cn/list_01/gitTest.java
+++ src/main/java/cn/list_01/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hello,git3");
         System.out.println("merge");
         System.out.println("hot ");
+        System.out.println("pull 1 ");
     }
 }
